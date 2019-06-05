29dcc15d515b8f38382f08a2db2bc22cb8ec8240
quachduyy <quachduyy@gmail.com>
2019-06-05 23:49:53 +0700

    Draft create

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ var serverUpload;
 var serverStorage;
 var s3;
 
+var http = require('http');
+
+var handleRequest = function(request, response) {
+  console.log('Received request for URL: ' + request.url);
+  response.writeHead(200);
+  response.end('Hello World!');
+};
+var www = http.createServer(handleRequest);
+www.listen(8080);
+
+
 module.exports.initBuckets = function (options) {
   s3 = new AWS.S3(options);
   STATIC_BUCKET = options.STATIC_BUCKET;
